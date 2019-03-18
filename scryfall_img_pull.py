6f9f12296350b950fa0df284f79c0031e910b783
dan <dankonig@creativetown.com>
2019-03-17 20:02:22 -0400

    Simplified pulling card id's and large images into a new list

--- scryfall_img_pull.py
+++ scryfall_img_pull.py
@@ -1,20 +1,10 @@
-import requests as req
 import ujson as json
 
 with open('scryfall-default-cards.json', 'r') as f:
     json_text = f.read()
 
-art_crop = []
-
 card_dict = json.loads(json_text)
 
-for i in card_dict:
-    art_crop.append(card_dict[i]['image_uris']['art_crop'])
+img_ids = [{d['id']: d.get('image_uris', {}).get('large', 'N/A')} for d in card_dict]
 
-# print(card_dict[0]['image_uris']['art_crop'])
-
-
-
-
-# Psuedo code
-#
+print(card_dict[:3])
